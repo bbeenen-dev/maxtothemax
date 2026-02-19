ce2e6c20ebe8eb5dfb4bb61fe901cb8006d01f46
Boudewijn Beenen <bbeenen@gmal.com>
2026-02-19 20:14:39 +0100

    2 kleine aanpassingen

--- components/update-password-form.tsx
+++ components/update-password-form.tsx
@@ -52,7 +52,7 @@ export function UpdatePasswordForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleForgotPassword}>
+          <form onSubmit={handleUpdatePassword}>
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="password">New password</Label>
